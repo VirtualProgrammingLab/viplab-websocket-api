80f745d177b38e09398dd0b215d65d9e9f3dc1f7
Leon Kiefer <leon.k97@gmx.de>
2019-11-30 21:08:54 +0100

    added CleanupTask to NotificationService

--- websocket-api-impl/src/main/java/de/uni_stuttgart/tik/viplab/websocket_api/NotificationService.java
+++ websocket-api-impl/src/main/java/de/uni_stuttgart/tik/viplab/websocket_api/NotificationService.java
@@ -1,36 +1,69 @@
 package de.uni_stuttgart.tik.viplab.websocket_api;
 
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import javax.enterprise.context.ApplicationScoped;
 import javax.websocket.Session;
 
 import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.annotation.Gauge;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 
+/**
+ * This NotificationService handles WebSocket sessions and make it possible to
+ * subscribe to events on a per session basis. This implementation is
+ * Thread-safe.
+ */
 @ApplicationScoped
 public class NotificationService {
 	private ConcurrentHashMap<String, Set<Session>> subscriptions = new ConcurrentHashMap<>();
-	
-	@Gauge(unit = MetricUnits.NONE)
+
+	@Resource
+	private ScheduledExecutorService executorService;
+
+	@Gauge(name= "topics-count", unit = MetricUnits.NONE)
 	public int getTopicCount() {
 		return subscriptions.size();
+	}
+
+	@PostConstruct
+	private void startCleanupTask() {
+		executorService.scheduleAtFixedRate(this::cleanUpSubscriptions, 0, 1, TimeUnit.MINUTES);
+	}
+
+	@Timed(name = "subscriptions-cleanup")
+	protected void cleanUpSubscriptions() {
+		this.subscriptions.forEach((topic, sessions) -> sessions.stream().filter(session -> !session.isOpen())
+				.forEach(session -> this.unsubscribe(topic, session)));
 	}
 
 	public void subscribe(String topic, Session session) {
 		subscriptions.compute(topic, (t, sessions) -> {
 			if (sessions == null) {
-				sessions = new HashSet<Session>();
+				sessions = new CopyOnWriteArraySet<Session>();
 			}
 			sessions.add(session);
 			return sessions;
 		});
 	}
 
+	/**
+	 * Subscribe the session the the given topic. This method is idempotent.
+	 * 
+	 * @param topic
+	 *            the topic of the subscription, topics can be structured
+	 *            hierarchical by separating the levels using a {@code :}.
+	 * @param session
+	 *            the WebSocket Session.
+	 */
 	public void unsubscribe(String topic, Session session) {
 		subscriptions.compute(topic, (t, sessions) -> {
 			if (sessions != null) {
