d57151a107237c64faeb05c60121c52e399151be
Leon Kiefer <leon.k97@gmx.de>
2020-03-30 19:26:08 +0200

    added sending to amqp close #3

--- amqp-connector/src/main/java/de/uni_stuttgart/tik/viplab/websocket_api/amqp/AMQPConnector.java
+++ amqp-connector/src/main/java/de/uni_stuttgart/tik/viplab/websocket_api/amqp/AMQPConnector.java
@@ -2,6 +2,11 @@ package de.uni_stuttgart.tik.viplab.websocket_api.amqp;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
 
 import de.uni_stuttgart.tik.viplab.websocket_api.ViPLabBackendConnector;
 import de.uni_stuttgart.tik.viplab.websocket_api.model.Computation;
@@ -13,14 +18,20 @@ import de.uni_stuttgart.tik.viplab.websocket_api.transformation.ComputationMerge
 public class AMQPConnector implements ViPLabBackendConnector {
 
 	@Inject
+	@Channel("computations")
+	private Emitter<String> computations;
+
+	@Inject
 	private ComputationMerger merger;
+
+	private Jsonb jsonb = JsonbBuilder.create();
 
 	@Override
 	public String createComputation(ComputationTemplate template, ComputationTask task) {
-
-
 		Computation computation = merger.merge(template, task);
-		
+		String computationJson = jsonb.toJson(computation);
+		computations.send(computationJson);
+
 		System.out.println("Template: " + template.identifier);
 		System.out.println("Task: " + task.identifier);
 		System.out.println("Computation: " + computation.identifier);
