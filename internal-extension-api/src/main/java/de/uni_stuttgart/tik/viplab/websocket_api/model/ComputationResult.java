711465242ddf9ec6332003147b2db824551e0751
Per Pascal Seeland <pascal.seeland@flightmare.net>
2020-08-13 20:26:49 +0200

    Adapt model to new json spec

--- internal-extension-api/src/main/java/de/uni_stuttgart/tik/viplab/websocket_api/model/ComputationResult.java
+++ internal-extension-api/src/main/java/de/uni_stuttgart/tik/viplab/websocket_api/model/ComputationResult.java
@@ -4,6 +4,11 @@ import java.util.Collections;
 import java.util.List;
 
 public class ComputationResult {
+
+	public String identifier;
+
+	public String version;
+
 	/**
 	 * The identifier of corresponding Computation
 	 */
@@ -14,31 +19,51 @@ public class ComputationResult {
 
 	public String timestamp;
 
-	public Notification notifications;
-
 	public Output output;
 
-	public List<File> files = Collections.emptyList();
+	public List<Artifact> artifacts = Collections.emptyList();
 
-	public static class Notification {
-		public String summary;
-
-		public List<Element> elements = Collections.emptyList();
-
-		public static class Element {
-			public String severity;
-			public String type;
-			public String message;
-		}
-	}
 
 	public static class Output {
 		public String stdout;
 		public String stderr;
 	}
 
-	public static class File {
+	public static class Artifact {
+
+		enum TYPE {
+			notifications,
+			file,
+			s3file
+		}
+
 		public String identifier;
+		public TYPE type;
+	}
+
+	public static class Notifications extends Artifact{
+
+		public Notifications() {
+			type = TYPE.notifications;
+		}
+
+		public String summary;
+
+		public List<Notification> notifications = Collections.emptyList();
+
+		public static class Notification {
+			public String severity;
+			public String type;
+			public String message;
+		}
+	}
+
+	public static class File extends Artifact{
+
+		public File() {
+			type = TYPE.file;
+		}
+
 		public String path;
 		public String MIMEtype;
 		public String content;
